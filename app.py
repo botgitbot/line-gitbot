1bdd085de101c56654efb5265e71fc3918ea5f70
addinnabilal <16520342@std.stei.itb.ac.id>
2022-06-06 22:40:22 +0700

    test saving text from user to variable

--- app.py
+++ app.py
@@ -1,52 +1,6 @@
-# import os
-# from flask import Flask, request, abort
-
-# from linebot import (
-#     LineBotApi, WebhookHandler
-# )
-# from linebot.exceptions import (
-#     InvalidSignatureError
-# )
-# from linebot.models import (
-#     MessageEvent, TextMessage, TextSendMessage,
-# )
-
-# app = Flask(__name__)
-
-# line_bot_api = LineBotApi('YOUR_CHANNEL_ACCESS_TOKEN')
-# handler = WebhookHandler('YOUR_CHANNEL_SECRET')
-
-
-# @app.route("/callback", methods=['POST'])
-# def callback():
-#     # Get X-Line-Signature header value
-#     signature = request.headers['X-Line-Signature']
-
-#     # Get request body as text
-#     body = request.get_data(as_text=True)
-#     app.logger.info("Request body: " + body)
-
-#     # Handle webhook body
-#     try:
-#         handler.handle(body, signature)
-#     except InvalidSignatureError:
-#         abort(400)
-
-#     return 'OK'
-
-
-# @handler.add(MessageEvent, message=TextMessage)
-# def handle_message(event):
-#     """ Here's all the messages will be handled and processed by the program """
-#     line_bot_api.reply_message(
-#         event.reply_token,
-#         TextSendMessage(text=event.message.text))
-
-
-# if __name__ == "__main__":
-#     port = int(os.environ.get('PORT', 5000))
-#     app.run(host='0.0.0.0', port=port)
-
+'''
+    IMPORT
+'''
 from flask import Flask, request, abort
 
 from linebot import (
@@ -59,10 +13,24 @@ from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage,
 )
 
+
+'''
+    CREATE FLASK
+'''
+
 app = Flask(__name__)
+
+'''
+    GLOBAL VARIABLE
+'''
 
 line_bot_api = LineBotApi('hBs3vE924/xltPVsO3ef+5Jz0Fn7nCUS7LiDlaoI9C89tMv0oha23N/BpyV4yrKmCtdP0VuBTPNuXTLjse7yGNdqSdb9+iOk9M0SHfZOhLzbcdQzB/LP4oDiEVxKz6BOp0X+lZ2noXKdwvY/Pj44BwdB04t89/1O/w1cDnyilFU=')
 handler = WebhookHandler('f85e13929825bf9df787c225af107155')
+
+
+'''
+    FLASK FUNCTION
+'''
 
 # 監聽所有來自 /callback 的 Post Request
 @app.route("/callback", methods=['POST'])
@@ -79,17 +47,39 @@ def callback():
         abort(400)
     return 'OK'
 
+
+var="default"
+
 # 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+
+    # get message yang diketikin sama user. simpen ke dalem variable msg_from_user
     msg_from_user = event.message.text
-    if (msg_from_user=="test"):
+    
+    # do something dengan bergantung sama messsage yang di chat sama user.
+    if msg_from_user=="test": # memastikan flask berjalan dengan aman
         line_bot_api.reply_message(
             event.reply_token,
-            TextSendMessage(text="test123"))
-    else:
-        message = TextSendMessage(text=event.message.text)
+            TextSendMessage(text="flask app is running!"))
+    elif msg_from_user=="tesvar": # test buat nyimpen variable
+        message = TextSendMessage(text=var)
         line_bot_api.reply_message(event.reply_token, message)
+    else: # kalau messagenya != "test", nyimpen text ke variable var
+        var=msg_from_user
+        message = TextSendMessage(text="berhasil menyimpan" + var + "!")
+        line_bot_api.reply_message(event.reply_token, message)
+
+
+'''
+    HELPER FUNCTION
+'''
+
+
+
+'''
+    RUN FLASK APP
+'''
 
 import os
 if __name__ == "__main__":
