0ae1de2de8e2e568fe59fc95412c999d68c6e089
Christopher Jeffrey <christopherjeffrey492@gmail.com>
2022-06-07 14:46:31 +0700

    delete debug sama var. delete debug soalnya udah bisa run local, bisa di print langsung. delete var soalnya tujuan sudah tercapai (proof of concept doang, bisa gk ngesimpen ke variable).  add kode buat 'something to run setiap berapa lama', karena kalo ngambil commit, probably caranya gitu, ada function yang dipanggil setiap berapa lama, yang manggil ke github api. di comment dulu tapi, fokus ke followers sama event dulu aja

--- app.py
+++ app.py
@@ -27,11 +27,6 @@ app = Flask(__name__)
 line_bot_api = LineBotApi('hBs3vE924/xltPVsO3ef+5Jz0Fn7nCUS7LiDlaoI9C89tMv0oha23N/BpyV4yrKmCtdP0VuBTPNuXTLjse7yGNdqSdb9+iOk9M0SHfZOhLzbcdQzB/LP4oDiEVxKz6BOp0X+lZ2noXKdwvY/Pj44BwdB04t89/1O/w1cDnyilFU=')
 handler = WebhookHandler('f85e13929825bf9df787c225af107155')
 
-var="default"
-
-consolelog = ""
-
-arrOfNumber = [1,2,3,4,5]
 
 
 '''
@@ -55,12 +50,11 @@ def callback():
 
 
 
-# 處理訊息
+# handle message
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     # KAMUS
     global var
-    global consolelog
     global arrOfNumber
 
     # ALGORITHM
@@ -72,53 +66,26 @@ def handle_message(event):
     if msg_from_user=="test": # memastikan flask berjalan dengan aman
         line_bot_api.reply_message(
             event.reply_token,
-            TextSendMessage(text="flask app is running with event info and followers edited with array to string!"))
-    elif msg_from_user=="tesvar": # test buat nyimpen variable
-        message = TextSendMessage(text="var = " + var)
-        line_bot_api.reply_message(event.reply_token, message)
+            TextSendMessage(text="flask app is running!"))
 
     elif msg_from_user == "followers":
         followers = line_bot_api.get_followers_ids()
-
-        # string_to_send = ""
-        # string_to_send += "tipe dari followers adalah " + type(followers) + "\n"
-        # string_to_send += "asumsi dia objek, atributnya adalah" + getattr(followers) + "\n"
-        # string_to_send += "followers =  " + str(followers) + "\n"
-
-        # consolelog += string_to_send
-
-        # message = TextSendMessage(text = string_to_send)
-        message = TextSendMessage(text = "user id ke 0 adalah " + followers.user_ids[0])
+        print("followers: " + str(followers))
+        message = TextSendMessage(text = "followers is printed in console")
         line_bot_api.reply_message(event.reply_token, message)
 
     elif msg_from_user == "eventinfo":
-         
-        string_to_send = ""
-        string_to_send += "tipe dari event adalah " + str(type(event)) + "\n"
-        string_to_send += "asumsi dia objek, atributnya adalah" + str(getattr(event)) + "\n"
-        string_to_send += "event =  " + str(event) + "\n"
-
-        consolelog += string_to_send
-
-        message = TextSendMessage(text = string_to_send)
+        print("eventinfo: " + str(event))
+        message = TextSendMessage(text = "event is printed in console")
         line_bot_api.reply_message(event.reply_token, message)
 
 
     elif msg_from_user == "printarray":
-        message = TextSendMessage(text = "array of number = " + str(arrOfNumber))
+        message = TextSendMessage(text = "array of number = " + str(["1","2","3","4","5"]))
         line_bot_api.reply_message(event.reply_token, message)
     
-    # FOR DEBUGGING PURPOSE
-    elif msg_from_user == "debug":
-        message = TextSendMessage(text = consolelog)
-        line_bot_api.reply_message(event.reply_token, message)
-    elif msg_from_user == "cleardebug":
-        consolelog = ""
-        message = TextSendMessage(text = "consolelog is cleared")
-        line_bot_api.reply_message(event.reply_token, message)
-    else: # kalau messagenya != "test", nyimpen text ke variable var
-        var=msg_from_user
-        message = TextSendMessage(text="berhasil menyimpan " + var + "!")
+    else: 
+        message = TextSendMessage(text="bukan command khusus!")
         line_bot_api.reply_message(event.reply_token, message)
 
 
@@ -126,6 +93,10 @@ def handle_message(event):
     HELPER FUNCTION
 '''
 
+def getNewCommit(repo_id):
+    # repo_id is identifier buat 'repo yang mana'
+    # return array of new commit. kalo ga ada, return empty array
+    pass
 
 
 '''
@@ -136,3 +107,27 @@ import os
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
+
+
+
+
+'''
+    FUNCTION TO RUN EVERY MINUTE
+'''
+
+# import time
+# import atexit
+
+# from apscheduler.schedulers.background import BackgroundScheduler
+
+
+# def print_date_time():
+#     print(time.strftime("%A, %d. %B %Y %I:%M:%S %p"))
+
+
+# scheduler = BackgroundScheduler()
+# scheduler.add_job(func=print_date_time, trigger="interval", seconds=60)
+# scheduler.start()
+
+# # Shut down the scheduler when exiting the app
+# atexit.register(lambda: scheduler.shutdown())
