4fb9fc6749860d8c6bf086225c666f1e0cbec143
addinnabilal <16520342@std.stei.itb.ac.id>
2022-06-18 21:31:23 +0700

    enable only group chat and refactor data structure

--- lineUtils.py
+++ lineUtils.py
@@ -16,8 +16,8 @@ line_bot_api = LineBotApi(config['LINE_CHANNEL_ACCESS_TOKEN'])
 # awalnya fungsi dibawah namanya 'chatToFollower'. aku ganti jadi sendString biar 1. lebih explisit(yang aku kirim string. keknya bisa selain string deh, dan kalo pake kata chat, terlalu luas jdinya) 2. bisa bedain antara 'send langsung sama reply'(karena keduanya termasuk chat)
 
 # mungkin next stepnya, buat ningkatin abstraksi, bisa dibikin fungsi chat() yang palugada, all in one.
-def sendStringToFollower(follower_id, string_to_send):
-    line_bot_api.push_message(follower_id, TextSendMessage(text=string_to_send))
+def sendStringToGroup(group_id, string_to_send):
+    line_bot_api.push_message(group_id, TextSendMessage(text=string_to_send))
 
 def replyString(reply_token, string_to_send):
     line_bot_api.reply_message(reply_token, TextSendMessage(text=string_to_send))
--- routineFunctions.py
+++ routineFunctions.py
@@ -2,43 +2,41 @@ import pytz
 from datetime import datetime
 
 from globalVariable import database
-from lineUtils import sendStringToFollower
+from lineUtils import sendStringToGroup
 from utils import (
     fetchFromGithub,
     diffOfTimeLessThanEqualToInterlude
     )
 
 def checkAndSendMessageIfEventHappensInAllRepo():
-    follower_id_arr = list(database.keys())
-    for follower_id in follower_id_arr:
-        print("follower id: " + follower_id)
-        print("username: " + database[follower_id]["username"])
-        print("access token: " + database[follower_id]["access_token"])
-        for repo in database[follower_id]["repos"]:
-            print("repo: " + repo)
-            results = filterEventsToGetNewEventsOnly(repo, database[follower_id]["username"], database[follower_id]["access_token"])
+    group_id_arr = list(database.keys())
+    for group_id in group_id_arr:
+        repo_arr = list(database[group_id].keys())
+        for usernameandrepo in repo_arr:
+            print("usernameandrepo: " + usernameandrepo)
+            access_token = database[group_id][usernameandrepo]["access_token"]
+            results = filterEventsToGetNewEventsOnly(usernameandrepo, access_token)
             if(len(results) != 0):
                 print("ada event baru")
                 for result in results:
                     if result["type"] == "PushEvent":
-                        # {result["actor"]["login"]} pushed to branch {result["payload"]["ref"]} {result["payload"]["commits"][0]["url"]}
-                        string_to_chat = f"[{repo}] {result['actor']['login']} pushed to branch {result['payload']['ref']} with {result['payload']['size']} commits:"
+                        string_to_chat = f"[{usernameandrepo}] {result['actor']['login']} pushed to branch {result['payload']['ref']} with {result['payload']['size']} commits:"
                         print(string_to_chat)
                         for commit in result["payload"]["commits"]:
                             string_to_chat += f"\n  - {commit['message']}"
-                        sendStringToFollower(follower_id, string_to_chat)
+                        sendStringToGroup(group_id, string_to_chat)
             else:
                 print("tidak ada event baru")
-                # chatToFollower(follower_id, "tidak ada event baru")
+                # chatTogroup(group_id, "tidak ada event baru")
 
 
-def filterEventsToGetNewEventsOnly(repo, username, access_token):
+def filterEventsToGetNewEventsOnly(usernameandrepo, access_token):
     # repo sama username buat ngeidentifikasi repo yang mana yang mo disentuh
     # return array of new event(new disini artinya event tersebut dilakukan selama interlude). kalo ga ada, return empty array
     
     # print("banyak event sampe saat ini:", len(res_dicts))
     start_time = datetime.now(pytz.utc).replace(tzinfo=None) 
-    responds = fetchFromGithub(username+"/"+repo, access_token)
+    responds = fetchFromGithub(usernameandrepo, access_token)
     # print("waktu sekarang", start_time)
     # iterasi setiap response, jika ada response yang dibuat < 5 menit terakhir, add to results
     results = []
