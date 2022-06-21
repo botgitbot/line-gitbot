6d700599f385bca508acd00a61dad6709e490a4b
Christopher Jeffrey <christopherjeffrey492@gmail.com>
2022-06-21 18:39:33 +0700

    pindah firebae jadi pake env, bukan file json lagi

--- app.py
+++ app.py
@@ -1,95 +1,87 @@
 # IMPORT KAMUS(env, config, globalvariable)
 from dotenv import load_dotenv
 load_dotenv()
-# # now you can use value from .env with from `os.environ` or `os.getenv`
+# now you can use value from .env with from `os.environ` or `os.getenv`
 import config 
-# import globalVariable
-# globalVariable.initialize()
+import globalVariable
+globalVariable.initialize()
 
 
-# # IMPORT SUBPROGRAM
+# IMPORT SUBPROGRAM
 from flask import Flask, request, abort
 import os
-# from linebot import (
-#  WebhookHandler
-# )
-# from linebot.exceptions import (
-#     InvalidSignatureError
-# )
-# from linebot.models import (
-#     MessageEvent,
-#     TextMessage,
-# )
+from linebot import (
+ WebhookHandler
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+from linebot.models import (
+    MessageEvent,
+    TextMessage,
+)
 
-# from eventBasedFunctions import replyBasedOnMessage
-# from routineFunctions import checkAndSendMessageIfEventHappensInAllRepo
+from eventBasedFunctions import replyBasedOnMessage
+from routineFunctions import checkAndSendMessageIfEventHappensInAllRepo
 
 
-# from firebaseUtils import setDatabaseFromFirebase
+from firebaseUtils import setDatabaseFromFirebase
 
 
 #    CREATE FLASK APP
 app = Flask(__name__)
 
 # SETUP LINE HANDLER
-# handler = WebhookHandler(os.getenv('LINE_CHANNEL_SECRET'))
+handler = WebhookHandler(os.getenv('LINE_CHANNEL_SECRET'))
 
 # SETUP DATABASE
 # when the app is first run, the database should be matching what's on firebase
-# setDatabaseFromFirebase()
+setDatabaseFromFirebase()
 
 #    FLASK FUNCTION
 
-# /
-@app.route('/')
-def hello_world():
-    return 'Hello World!' + os.getenv('TEST_ENV')
-
-
-
-
 # ini route yang dipake saat pertama kali nge connect in ke line dev
-# @app.route("/callback", methods=['POST'])
-# def callback():
-#     # get X-Line-Signature header value
-#     signature = request.headers['X-Line-Signature']
-#     # get request body as text
-#     body = request.get_data(as_text=True)
-#     app.logger.info("Request body: " + body)
-#     # handle webhook body
-#     try:
-#         handler.handle(body, signature)
-#     except InvalidSignatureError:
-#         abort(400)
-#     return 'OK'
+@app.route("/callback", methods=['POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+    return 'OK'
 
-# # handle message
-# @handler.add(MessageEvent, message=TextMessage)
-# def handle_message(event):
-#     # 'strip down' event, to get only the data we need
-#     msg_from_user = event.message.text
-#     reply_token = event.reply_token
-#     source_type = event.source.type
-#     source_id = event.source.user_id
-#     followers_id = event.source.user_id
+# handle message
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    # 'strip down' event, to get only the data we need
+    msg_from_user = event.message.text
+    reply_token = event.reply_token
+    source_type = event.source.type
+    source_id = event.source.user_id
+    followers_id = event.source.user_id
 
-#     if (source_type == 'user'):
-#         source_id = event.source.user_id
-#     elif (event.source.type == 'group'):
-#         source_id = event.source.group_id
+    if (source_type == 'user'):
+        source_id = event.source.user_id
+    elif (event.source.type == 'group'):
+        source_id = event.source.group_id
 
-#     replyBasedOnMessage(msg_from_user, source_type, source_id, reply_token)
+    replyBasedOnMessage(msg_from_user, source_type, source_id, reply_token)
 
 
-# #   FUNCTION TO RUN EVERY MINUTE
-# import atexit
-# from apscheduler.schedulers.background import BackgroundScheduler
+#   FUNCTION TO RUN EVERY MINUTE
+import atexit
+from apscheduler.schedulers.background import BackgroundScheduler
 
-# scheduler = BackgroundScheduler()
-# scheduler.add_job(func=checkAndSendMessageIfEventHappensInAllRepo, trigger="interval", seconds = config.INTERLUDE)
-# scheduler.start()
-# # Shut down the scheduler when exiting the app
-# atexit.register(lambda: scheduler.shutdown())
+scheduler = BackgroundScheduler()
+scheduler.add_job(func=checkAndSendMessageIfEventHappensInAllRepo, trigger="interval", seconds = config.INTERLUDE)
+scheduler.start()
+# Shut down the scheduler when exiting the app
+atexit.register(lambda: scheduler.shutdown())
 
 
 #    RUN FLASK APP
--- firebaseUtils.py
+++ firebaseUtils.py
@@ -6,11 +6,27 @@ import os
 import firebase_admin
 from firebase_admin import credentials
 
+# create firebaseCredential based on env
+fc = {
+    "type": os.getenv("FIREBASE_CREDENTIAL_TYPE"),
+    "project_id": os.getenv("FIREBASE_CREDENTIAL_PROJECT_ID"),
+    "private_key_id": os.getenv("FIREBASE_CREDENTIAL_PRIVATE_KEY_ID"),
+    "private_key": os.getenv("FIREBASE_CREDENTIAL_PRIVATE_KEY"),
+    "client_email": os.getenv("FIREBASE_CREDENTIAL_CLIENT_EMAIL"),
+    "client_id": os.getenv("FIREBASE_CREDENTIAL_CLIENT_ID"),
+    "auth_uri": os.getenv("FIREBASE_CREDENTIAL_AUTH_URI"),
+    "token_uri": os.getenv("FIREBASE_CREDENTIAL_TOKEN_URI"),
+    "auth_provider_x509_cert_url": os.getenv("FIREBASE_CREDENTIAL_AUTH_PROVIDER_X509_CERT_URL"),
+    "client_x509_cert_url": os.getenv("FIREBASE_CREDENTIAL_CLIENT_X509_CERT_URL")
+}
+
+
+
 
 # di firebase, satu buah project bisa terdiri atas beberapa app. kita cuman pake satu
 # once the code below this comment is run, a new app will be created. that app doesn't have to be stored in a variable. I'm not quite sure how it works underneath, but let's assume the data is stored inside the firebase admin library
 # by importing firebaseutils in app, code nya di run(emang gitu cara kerja python)
-cred = credentials.Certificate("firebaseCredential.json")
+cred = credentials.Certificate(fc)
 # certificate = credentials.Certificate(config['FIREBASE_KEY_API'])
 firebase_admin.initialize_app(cred,{
     'databaseURL': os.getenv("FIREBASE_DATABASE_URL")
