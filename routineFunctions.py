fd408e80ea7dbf9261cb6d567d1132bcea555132
addinnabilal <16520342@std.stei.itb.ac.id>
2022-06-25 11:51:11 +0700

    add release, pull request review comment respond and change pull request respond

--- routineFunctions.py
+++ routineFunctions.py
@@ -15,33 +15,29 @@ def checkAndSendMessageIfEventHappensInAllRepo():
         for usernameandrepo in repo_arr:
             print("usernameandrepo: " + usernameandrepo)
             access_token = globalVariable.database[group_id][usernameandrepo]["access_token"]
-
+            usernameandrepo = usernameandrepo.replace("@", "/")
 
             #EVENT
-            results = filterEventsToGetNewEventsOnly(usernameandrepo.replace("@", "/"), access_token)
+            results = filterEventsToGetNewEventsOnly(usernameandrepo, access_token)
             if(len(results) != 0):
                 print("ada event baru di", usernameandrepo)
                 for event in results:
                     respondBasedOnEvent(usernameandrepo, event, group_id)
             else:
                 print("tidak ada event baru di", usernameandrepo)
-            #EVENT
-            # PULL
-
-
-            # PULL
-
-
-
-
 
 
 def respondBasedOnEvent(usernameandrepo, event, group_id):
-    if event["type"] == "PushEvent":
+    event_type = event["type"]
+    if event_type == "PushEvent":
         respondOnPushEvent(usernameandrepo, event, group_id)
 
-    elif event["type"] == "PullRequestEvent":
+    elif event_type == "PullRequestEvent":
         respondOnPullRequestEvent(usernameandrepo, event, group_id)
+    elif event_type == "ReleaseEvent" and event["payload"]["action"] == "published" and not(event["payload"]["prerelease"]):
+        respondOnReleaseEvent(usernameandrepo, event, group_id)
+    elif event_type == "PullRequestReviewCommentEvent":
+        respondOnPullRequestReviewCommentEvent(usernameandrepo, event, group_id)
     # elif
 
 def respondOnPushEvent(usernameandrepo, event, group_id):
@@ -52,7 +48,9 @@ def respondOnPushEvent(usernameandrepo, event, group_id):
     sendStringToGroup(group_id, string_to_chat)
 
 def respondOnPullRequestEvent(usernameandrepo, event, group_id):
-    string_to_chat = f"[{usernameandrepo}] {event['actor']['login']} {event['payload']['action']} pull request {event['payload']['pull_request']['title']}"
+    string_to_chat = f"[{usernameandrepo}] {event['actor']['login']} {event['payload']['action']} pull request \"{event['payload']['pull_request']['title']}\" from {event['payload']['pull_request']['head']['ref']} to {event['payload']['pull_request']['base']['ref']}"
+    print(string_to_chat)
+    sendStringToGroup(group_id, string_to_chat)
     if event['actor']['login'] == "opened" and event['payload']['pull_request']['requested_reviewers']:
         string_to_chat += " with request to review from: "
         for reviewer in event['payload']['pull_request']['requested_reviewers']:
@@ -60,6 +58,14 @@ def respondOnPullRequestEvent(usernameandrepo, event, group_id):
     print(string_to_chat)
     sendStringToGroup(group_id, string_to_chat)
 
+def respondOnReleaseEvent(usernameandrepo, event, group_id):
+    string_to_chat = f"[{usernameandrepo}] {event['actor']['login']} released tag {event['payload']['release']['tag_name']} \"{event['payload']['release']['name']}\""
+    print(string_to_chat)
+    sendStringToGroup(group_id, string_to_chat)
+
+def respondOnPullRequestReviewCommentEvent(usernameandrepo, event, group_id):
+    string_to_chat = f"[{usernameandrepo}] {event['actor']['login']} commented on pull request \"{event['payload']['pull_request']['title']}\""
+    sendStringToGroup(group_id, string_to_chat)
 
 
 def filterEventsToGetNewEventsOnly(usernameandrepo, access_token):
