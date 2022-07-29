d252e01768dec7ec97f587dd242657f6d74e0478
Christopher Jeffrey <christopherjeffrey492@gmail.com>
2022-07-30 01:04:56 +0700

    benerin show repo

--- handleLineEvent/handleShowRepo.py
+++ handleLineEvent/handleShowRepo.py
@@ -8,11 +8,11 @@ def handleShowRepo(reply_token, group_id):
 
 
 
-    if group_id in list(globalVariable.database.active.keys()):
+    if group_id in list(globalVariable.database["active"].keys()):
         stringToSend = ""
-        for key in globalVariable.database.active[group_id]:
-            username = globalVariable.database.active[group_id][key]["username"]
-            repo = globalVariable.database.active[group_id][key]["repo"]
+        for key in globalVariable.database["active"][group_id]:
+            username = globalVariable.database["active"][group_id][key]["username"]
+            repo = globalVariable.database["active"][group_id][key]["repo"]
             stringToSend += username + ": " + repo + "\n"
         replyFlexMessageTemplateTitleText(reply_token, "Repository list in this group: ", stringToSend)
     else:
