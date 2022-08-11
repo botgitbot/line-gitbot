9cbd5f5ac4cc66a136cabbbde80c99e026039bbe
addinnabilal <16520342@std.stei.itb.ac.id>
2022-08-11 23:17:46 +0700

    fix commit message on pull req event ; update help message

--- handleLineEvent/handleSendHelp.py
+++ handleLineEvent/handleSendHelp.py
@@ -2,6 +2,6 @@
 from utils.lineUtils import replyFlexMessageTemplateTitleText
 
 def handleSendHelp(reply_token):
-    replyFlexMessageTemplateTitleText(reply_token,"Try sending these message!", "!help - show help message\n!add - give a link that can be added to your repository's webhook to track the repository\n!show - show all tracked repositories\n!manualdelete \{username}/\{repo\} - manually remove a repository from the list (notes: the repository is still tracked unless you remove the webhook url directly from github)")
+    replyFlexMessageTemplateTitleText(reply_token,"Try sending these message!", "!help - show help message\n!add - give a lurl that can be added to your repository's webhook to track the repository\n!show - show all tracked repositories\n!manualdelete {username}/{repo} - manually remove a repository from the list (notes: the repository will still be tracked unless you remove the webhook url directly from github)")
 
         
