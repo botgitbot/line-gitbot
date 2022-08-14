4b1e916eaaab305725e90e731e9819d68b587a0c
Addin Nabilal Huda <70847819+addinnabilal@users.noreply.github.com>
2022-08-14 19:52:31 +0700

    Change to prod URL

--- handleLineEvent/handleAddRepo.py
+++ handleLineEvent/handleAddRepo.py
@@ -7,6 +7,6 @@ from utils.utils import decryptGroupId, encryptGroupId
 def handleAddRepo(reply_token, group_id):
     # reply with a webhook link sesuai dengan groupid mereka (di encrypt dulu ye) formatnya https://domain.com/webhook/<encryptedgroupid>
     encrypted = encryptGroupId(group_id)
-    link = "https://line-gitbot.vercel.app/webhook/" + encrypted
+    link = "https://line.gitbot.my.id/webhook/" + encrypted
     replyString(reply_token, "Please add following URL to your repository webhook (Repository page > Settings > Webhooks > Add webhook):"  + "\n" + link)
     
