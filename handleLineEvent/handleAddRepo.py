7e3788434ebbb6959ff5a0a4a2d1c581f140d0c0
Christopher Jeffrey <christopherjeffrey492@gmail.com>
2022-10-15 20:37:25 +0700

    bugfix: !add missing one slash

--- handleLineEvent/handleAddRepo.py
+++ handleLineEvent/handleAddRepo.py
@@ -7,6 +7,6 @@ from utils.utils import decryptGroupId, encryptGroupId
 def handleAddRepo(reply_token, group_id):
     # reply with a webhook link sesuai dengan groupid mereka (di encrypt dulu ye) formatnya https://domain.com/webhook/<encryptedgroupid>
     encrypted = encryptGroupId(group_id)
-    link = os.getenv("BASE_URL") + encrypted
+    link = os.getenv("BASE_URL") + "/" +  encrypted
     replyString(reply_token, "Please add following URL to your repository webhook (Repository page > Settings > Webhooks > Add webhook):"  + "\n" + link)
     
